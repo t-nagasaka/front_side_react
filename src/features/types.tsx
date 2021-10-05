d586eef52213fd8ffb70e3173405f48b9250ffc1
t-nagasaka <66255777+t-nagasaka@users.noreply.github.com>
2021-10-05 21:20:24 +0900

    create_types

--- src/features/types.tsx
+++ src/features/types.tsx
@@ -1,4 +1,21 @@
+// 型情報をBlobからエクステンド
+export interface File extends Blob {
+  readonly lastModified: number;
+  readonly name: string;
+}
+
+// authSlice.ts/
 export interface PROPS_AUTHEN {
   email: string;
   password: string;
+}
+
+export interface PROPS_PROFILE {
+  id: number;
+  nickName: string;
+  img: File | null;
+}
+
+export interface PROPS_NICKNAME {
+  nickName: string;
 }

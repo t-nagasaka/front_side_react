b23a2a97971a26a043048957df4781b3c8ffd489
t-nagasaka <66255777+t-nagasaka@users.noreply.github.com>
2021-10-08 23:08:02 +0900

    create_reature/create_ahthComponente

--- src/app/store.ts
+++ src/app/store.ts
@@ -9,7 +9,6 @@ export const store = configureStore({
   },
 });
 
-export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
 export type AppThunk<ReturnType = void> = ThunkAction<
   ReturnType,
@@ -17,3 +16,5 @@ export type AppThunk<ReturnType = void> = ThunkAction<
   unknown,
   Action<string>
 >;
+
+export type AppDispatch = typeof store.dispatch;
